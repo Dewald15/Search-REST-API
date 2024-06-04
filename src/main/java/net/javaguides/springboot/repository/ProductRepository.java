edda2f6f60a5a2c75029a991658a18cb7cbf5a1e
Dewald15 <vdbergdewald@gmail.com>
2024-06-04 11:30:03 +0200

    Added a nativeQuery in ProductRepository as an example of the difference between JPQL queries

--- src/main/java/net/javaguides/springboot/repository/ProductRepository.java
+++ src/main/java/net/javaguides/springboot/repository/ProductRepository.java
@@ -12,4 +12,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             "p.name LIKE CONCAT('%',:query, '%')" +
             "Or p.description LIKE CONCAT('%', :query, '%')")
     List<Product> searchProducts(String query);
+
+//    @Query(value = "SELECT * FROM products p WHERE " +
+//            "p.name LIKE CONCAT('%',:query, '%')" +
+//            "Or p.description LIKE CONCAT('%', :query, '%')", nativeQuery = true)
+//    List<Product> searchProductsSQL(String query);
 }
